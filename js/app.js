c7386113baa1798d49724a849afd841326344842
halley.qiu <qiu.hao@whaley.cn>
2017-08-06 21:48:25 +0800

    添加课程目录列表、课程详情、选课详情、活动详情，添加vue.js绑定数据等等

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@
 	 */
 	//owner.hostname='http://api.course.com:8081/course-api';
 	
-	owner.hostname='http://192.168.0.185:8081/course-api';
+	owner.hostname='http://192.168.31.232:8081/course-api';
 	
 	/**
 	 * 用户登录
@@ -60,6 +60,10 @@
 			},
 			error:function(xhr,type,errorThrown){
 				//异常处理；
+				console.log(xhr);
+				console.log(type);
+				console.log(errorThrown);
+				
 				var msg={
 					code:-1,
 					msg:"用户名或密码错误"
@@ -68,19 +72,20 @@
 			}
 		});
 
-
-//		if (authed) {
-//			return owner.createState(loginInfo.account, callback);
-//		} else {
-//			return callback('用户名或密码错误');
-//		}
 	};
 
-	owner.createState = function(useInfo) {
+	owner.createState = function(userInfo) {
 		var state = owner.getState();
-		state.account = useInfo.loginName;
-		state.token = useInfo.id;
-		state.manageId = useInfo.manageId;
+		
+		
+		state.account = userInfo.loginName;
+		state.token = userInfo.id;
+		state.manageId = userInfo.manageId;
+		if(userInfo.roleType==3){
+			state.studentsDetail=userInfo.studentsDetail;
+		}else if(userInfo.roleType==2){
+			state.teacherDetail=userInfo.teacherDetail;
+		}
 		owner.setState(state); 
 	};
 	/**
@@ -96,6 +101,121 @@
 		window.location.href='../login.html'
 		
 	}
+
+	
+
+	/**
+	 * 设置当前状态
+	 **/
+	owner.setState = function(state) {
+		state = state || {};
+		localStorage.setItem('$state', JSON.stringify(state));
+		//var settings = owner.getSettings();
+		//settings.gestures = '';
+		//owner.setSettings(settings);
+	};
+
+	
+
+	/*
+	 * 课程-获取课程列表
+	 * */
+	owner.getCourseAll=function(pageSize,pageIndex,callback){
+		var userInfo = app.getState(); 
+		var url=owner.hostname+'/view/teachingManage/listData.shtml?manageId='+userInfo.manageId+'&pageSize='+pageSize+'&pageIndex='+pageIndex;
+		return owner.getCommon(url,callback);
+	}
+	
+	/*
+	 * 课程-获取课程详情
+	 * */
+	owner.getCourseDetail=function(chapId,courseId,callback){
+		var userInfo = app.getState(); 
+		var classId = userInfo.studentsDetail==undefined?0:userInfo.studentsDetail.stdClass;
+		var url=owner.hostname+'/view/teachingManage/detailView.shtml?chapClassId='+chapId+'&manageId='+userInfo.manageId+'&tmId='+courseId+'&tcId='+classId ;
+		return owner.getCommon(url,callback);
+	}
+	
+	/*
+	 * 课程-获取课程目录
+	 */	
+	owner.getCourseDir=function(rcId,parentId,callback){
+		var userInfo = app.getState(); 
+		// /view/teachingManage/listTreeData.shtml?manageId=55&rcId=4&id=5
+		var url=owner.hostname+'/view/teachingManage/listTreeData.shtml?manageId='+userInfo.manageId+'&rcId='+rcId+'&id='+parentId;
+	
+		return owner.getCommon(url,callback); 
+	}
+	
+	/*
+	 * 课程-获取课程选课
+	 */	
+	owner.getCourseSelect=function(rcId,callback){
+		var userInfo = app.getState();
+		// /view/teachingManage/listTreeData.shtml?manageId=55&rcId=4&id=5
+		var url=owner.hostname+'/view/teachingManage/courseData.shtml?manageId='+userInfo.manageId+'&rcId='+rcId; 
+		return owner.getCommon(url,callback); 
+	}
+	 
+	/*
+	  * 活动 -获取活动列表
+	  */
+	owner.getActivityAll=function(userInfo,pageSize,pageIndex,callback){		
+		var url=owner.hostname+'/view/activity/listData.shtml?manageId='+userInfo.manageId+'&pageSize='+pageSize+'&pageIndex='+pageIndex;
+		return owner.getCommon(url,callback);
+	}
+	
+	/*
+	  * 活动 -获取活动详情
+	  */
+	owner.getActivityDetail=function(activityId,callback){		
+//		var url=owner.hostname+'/view/activity/listData.shtml?manageId='+userInfo.manageId+'&pageSize='+pageSize+'&pageIndex='+pageIndex;
+//		return owner.getCommon(url,callback);
+		var msg={
+				code:-1,
+				msg:"获取数据失败！"
+			}
+		return callback(msg);
+	}
+	
+	
+	/*
+	  * 成果 -获取列表
+	  */
+	owner.getShowAll=function(userInfo,actFine,pageSize,pageIndex,callback){
+		//view/activity/activityShow.shtml?manageId=55&actFine=1
+		var url=owner.hostname+'/view/activity/activityShow.shtml?manageId='+userInfo.manageId+'&actFine='+actFine+'&pageSize='+pageSize+'&pageIndex='+pageIndex;
+		return owner.getCommon(url,callback); 
+	}
+	
+	
+	
+		/*
+	 * 业务功能
+	 * */
+	owner.getCommon=function(url,callback){
+			console.log(url);
+		mui.ajax( url,{
+			dataType:'json',//服务器返回json格式数据
+			crossDomain:true,
+			type:'get',//HTTP请求类型
+			timeout:10000,//超时时间设置为10秒； 
+	        contentType:"application/json; charset=utf-8",
+			headers:{'Content-Type':'application/json; charset=utf-8'},	              
+			success:function(data){
+				return callback(data);
+			},
+			error:function(xhr,type,errorThrown){
+				//异常处理；
+				var msg={
+					code:-1,
+					msg:"获取数据失败！"
+				}
+					return callback(msg);
+			}
+		});
+	}
+
 
 	/**
 	 * 新用户注册
@@ -119,52 +239,9 @@
 		localStorage.setItem('$users', JSON.stringify(users));
 		return callback();
 	};
-
 	
-
-	/**
-	 * 设置当前状态
-	 **/
-	owner.setState = function(state) {
-		state = state || {};
-		localStorage.setItem('$state', JSON.stringify(state));
-		//var settings = owner.getSettings();
-		//settings.gestures = '';
-		//owner.setSettings(settings);
-	};
-
-	var checkEmail = function(email) {
-		email = email || '';
-		return (email.length > 3 && email.indexOf('@') > -1);
-	};
-
-	/**
-	 * 找回密码
-	 **/
-	owner.forgetPassword = function(email, callback) {
-		callback = callback || $.noop;
-		if (!checkEmail(email)) {
-			return callback('邮箱地址不合法');
-		}
-		return callback(null, '新的随机密码已经发送到您的邮箱，请查收邮件。');
-	};
-
-	/**
-	 * 获取应用本地配置
-	 **/
-	owner.setSettings = function(settings) {
-		settings = settings || {};
-		localStorage.setItem('$settings', JSON.stringify(settings));
-	}
-
-	/**
-	 * 设置应用本地配置
-	 **/
-	owner.getSettings = function() {
-			var settingsText = localStorage.getItem('$settings') || "{}";
-			return JSON.parse(settingsText);
-		}
-		/**
+	
+			/**
 		 * 获取本地是否安装客户端
 		 **/
 	owner.isInstalled = function(id) {
@@ -201,56 +278,38 @@
 	}
 	
 	
-	
-	/*
-	 * 业务功能
-	 * */
-	owner.getCommon=function(url,callback){
-		mui.ajax( url,{			
-			dataType:'json',//服务器返回json格式数据
-			type:'get',//HTTP请求类型
-			timeout:10000,//超时时间设置为10秒； 
-	        //contentType:"application/x-www-form-urlencoded; charset=utf-8",
-			headers:{'Content-Type':'application/json; charset=utf-8'},	              
-			success:function(data){
-				return callback(data);
-			},
-			error:function(xhr,type,errorThrown){
-				//异常处理；
-				var msg={
-					code:-1,
-					msg:"获取数据失败！"
-				}
-					return callback(msg);
-			}
-		});
+	var checkEmail = function(email) {
+		email = email || '';
+		return (email.length > 3 && email.indexOf('@') > -1);
+	};
+
+	/**
+	 * 找回密码
+	 **/
+	owner.forgetPassword = function(email, callback) {
+		callback = callback || $.noop;
+		if (!checkEmail(email)) {
+			return callback('邮箱地址不合法');
+		}
+		return callback(null, '新的随机密码已经发送到您的邮箱，请查收邮件。');
+	};
+
+	/**
+	 * 获取应用本地配置
+	 **/
+	owner.setSettings = function(settings) {
+		settings = settings || {};
+		localStorage.setItem('$settings', JSON.stringify(settings));
 	}
 
-	/*
-	 * 课程-获取课程列表
-	 * */
-	owner.getCourseAll=function(userInfo,pageSize,pageIndex,callback){
-		var url=owner.hostname+'/view/teachingManage/listData.shtml?manageId='+userInfo.manageId+'&pageSize='+pageSize+'&pageIndex='+pageIndex;
-		return owner.getCommon(url,callback);
-	}
-	
-	/*
-	  * 活动 -获取活动列表
-	  */
-	owner.getActivityAll=function(userInfo,pageSize,pageIndex,callback){		
-		var url=owner.hostname+'/view/activity/listData.shtml?manageId='+userInfo.manageId+'&pageSize='+pageSize+'&pageIndex='+pageIndex;
-		return owner.getCommon(url,callback);
-	}
-	
-		/*
-	  * 成果 -获取列表
-	  */
-	owner.getShowAll=function(userInfo,actFine,pageSize,pageIndex,callback){
-		//view/activity/activityShow.shtml?manageId=55&actFine=1
-		var url=owner.hostname+'/view/activity/activityShow.shtml?manageId='+userInfo.manageId+'&actFine='+actFine+'&pageSize='+pageSize+'&pageIndex='+pageIndex;
-		return owner.getCommon(url,callback); 
-	}
-	
+	/**
+	 * 设置应用本地配置
+	 **/
+	owner.getSettings = function() {
+			var settingsText = localStorage.getItem('$settings') || "{}";
+			return JSON.parse(settingsText);
+		}
+
 	
 	
 }(mui, window.app = {}));
